cdb40fb6dc01dbc7f3efa449367410ceef0a05b0
Maicon Lourenço <maiconloure@gmail.com>
2023-10-14 18:03:32 -0300

    feat: Added User package

---
+++ src/main/java/com/maiconloure/todolist/user/UserController.java
@@ -0,0 +1,36 @@
+package com.maiconloure.todolist.user;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @PostMapping()
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists");
+        }
+
+        var passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashed);
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    }
+
+    @GetMapping()
+    public void get() {
+        System.out.println("User");
+    }
+}
